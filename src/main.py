1d4b00b1961ba81951566094503a1020f8e2f970
Riverbank6150 <williamcameronslighte@gmail.com>
2024-02-07 14:56:59 -0500

    Added func 'print_to_CSV' in main.py to produce csv file.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ def main():
     url = "https://www.basketball-reference.com/leagues/NBA_2023_per_game.html"
     soup = scrape_URL(url)
     parse(soup)
+
+    print_to_CSV()
     print_to_JSON()
     print_to_xlsx()
 
